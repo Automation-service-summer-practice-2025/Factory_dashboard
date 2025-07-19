864a66df91363e9da806115bd0eafd34ad2e10d4
portlexs <portnyagin924@gmail.com>
2025-07-19 12:41:00 +0600

    feat: add constructor which get station id from router

--- factory_dashboard/src/app/pages/station-details/station-details.component.ts
+++ factory_dashboard/src/app/pages/station-details/station-details.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-stations-details',
+  selector: 'app-station-details',
   imports: [],
   templateUrl: './station-details.component.html',
   styleUrls: ['./station-details.component.css']
 })
 export class StationDetailsComponent {
+  stationId!: number;
 
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      this.stationId = +params['id'];
+      console.log('Station ID:', this.stationId);
+    });
+  }
 }
