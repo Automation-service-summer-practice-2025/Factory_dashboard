70fb26c00d0d4a511c8b6aa36c665a6cccd77a7a
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-07-19 20:15:12 +0600

    feat: add factory-map

---
+++ factory_dashboard/src/app/pages/factory-map/factory-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factory-map',
+  imports: [],
+  templateUrl: './factory-map.html',
+  styleUrl: './factory-map.css'
+})
+export class FactoryMap {
+
+}
