4285e749f7aa0524ccc01661f04f459cce79c0ac
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-07-19 20:21:57 +0600

    feat: add path to map page

--- factory_dashboard/src/app/app.routes.ts
+++ factory_dashboard/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { StationDetailsComponent } from './pages/station-details/station-details.component';
+import { FactoryMap } from './pages/factory-map/factory-map';
 
 export const routes: Routes = [
   {
     path: 'station/:station_id',
     component: StationDetailsComponent
   },
+  {
+    path: '',
+    component: FactoryMap
+  }
 ];
