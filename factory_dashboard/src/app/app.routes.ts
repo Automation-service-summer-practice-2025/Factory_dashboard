e660b2200f2aabca9ac3b5ae56ca51f2db774b53
portlexs <portnyagin924@gmail.com>
2025-07-19 11:25:34 +0600

    feat: connect station details page to router and app

--- factory_dashboard/src/app/app.routes.ts
+++ factory_dashboard/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StationDetailsComponent } from './pages/station-details/station-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: 'station/:id',
+    component: StationDetailsComponent
+  },
+];
