9605646cb457a051a8d6047b88d58e44f13a9512
portlexs <portnyagin924@gmail.com>
2025-07-19 11:20:11 +0600

    feat: create station-details component

---
+++ factory_dashboard/src/app/pages/stations-details/stations-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stations-details',
+  imports: [],
+  templateUrl: './stations-details.component.html',
+  styleUrl: './stations-details.component.css'
+})
+export class StationsDetailsComponent {
+
+}
