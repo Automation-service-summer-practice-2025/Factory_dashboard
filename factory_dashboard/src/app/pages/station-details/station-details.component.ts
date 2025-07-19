da67277da563ede18f8c9b9bec717cf8a45e5d50
portlexs <portnyagin924@gmail.com>
2025-07-19 11:24:57 +0600

    chore: rename station-details component

---
+++ factory_dashboard/src/app/pages/station-details/station-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stations-details',
+  imports: [],
+  templateUrl: './station-details.component.html',
+  styleUrls: ['./station-details.component.css']
+})
+export class StationDetailsComponent {
+
+}
