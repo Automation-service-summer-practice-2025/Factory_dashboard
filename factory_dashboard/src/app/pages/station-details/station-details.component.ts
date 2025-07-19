271c69a6fa5330e8c8927ace4b6b7d4d95580431
portlexs <portnyagin924@gmail.com>
2025-07-19 15:13:08 +0600

    feat: add mock data loading

--- factory_dashboard/src/app/pages/station-details/station-details.component.ts
+++ factory_dashboard/src/app/pages/station-details/station-details.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { APIService } from '../../services/api.service';
+import { Station } from '../../models/station.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-station-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './station-details.component.html',
   styleUrls: ['./station-details.component.css']
 })
 export class StationDetailsComponent implements OnInit {
-  stationId!: number;
-  stationData: any;
+  station: Station | null = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -18,21 +19,53 @@ export class StationDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.stationId = +params['id'];
-      this.loadStationData();
+    const stationId = Number(this.route.snapshot.paramMap.get('station_id'));
+    // this.loadStationData(stationId);
+    this.loadStationMockData(stationId);
+    console.log("station data:", this.station)
+  }
+
+  loadStationData(stationId: number) {
+    this.apiService.getStationById(stationId).subscribe({
+      next: (data) => {
+        this.station = data;
+      },
+      error: (err) => {
+        console.error('Ошибка загрузки данных установки:', err);
+      }
     });
   }
 
-  loadStationData() {
-    this.apiService.getStationById(this.stationId).subscribe({
-      next: (data) => {
-        this.stationData = data;
-        console.log('Station data:', this.stationData);
-      },
-      error: (err) => {
-        console.error('Error fetching station data:', err);
-      }
-    });
+  loadStationMockData(stationId: number) {
+    this.station = {
+      station_id: 1,
+      station_name: "АТ-ВБ",
+      station_elements: [
+        {
+          element_id: 101,
+          element_name: "Насос H-21",
+          working_status: true,
+          checking_date_start: "2024-01-10T09:00:00",
+          checking_date_finish: "2024-01-15T17:00:00",
+          block_key_status: true
+        },
+        {
+          element_id: 102,
+          element_name: "Клапан K-45",
+          working_status: true,
+          checking_date_start: "2024-02-15T10:00:00",
+          checking_date_finish: "2024-02-20T16:00:00",
+          block_key_status: false
+        },
+        {
+          element_id: 103,
+          element_name: "Датчик Д-12",
+          working_status: false,
+          checking_date_start: "2024-03-20T11:00:00",
+          checking_date_finish: "2024-03-25T15:00:00",
+          block_key_status: true
+        }
+      ]
+    };
   }
 }
