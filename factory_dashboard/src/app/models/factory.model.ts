d58c12ca9214f0fa18e087b9f8a6a01ecb45000b
portlexs <portnyagin924@gmail.com>
2025-07-23 14:22:00 +0600

    fix: delete params from stationEquipmentModel

--- factory_dashboard/src/app/models/factory.model.ts
+++ factory_dashboard/src/app/models/factory.model.ts
@@ -22,18 +22,16 @@ export interface StationEquipmentModel {
   element_id: number;
   element_name: string;
   station_id: number;
-  params: {
-    manufacturer: string;
-    passport: number;
-    factory: number;
-    certificate: string;
-    interval_check: number;
-    last_metrological_control: Date;
-    last_check: Date;
-    range_measurement: string;
-    block_key_status: boolean;
-    working_status: boolean;
-  };
+  manufacturer: string;
+  passport: number;
+  factory: number;
+  certificate: string;
+  interval_check: number;
+  last_metrological_control: Date;
+  last_check: Date;
+  range_measurement: string;
+  block_key_status: boolean;
+  working_status: boolean;
   documents?: Documents[];
   checking_date_start: string;
   checking_date_end: string;
