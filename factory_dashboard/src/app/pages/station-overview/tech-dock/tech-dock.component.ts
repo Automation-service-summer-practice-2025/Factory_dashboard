5275f2fcdcc0397046bfd355c4ebe21ee737e232
KiriLLchik07 <kirill3456777@mail.ru>
2025-07-23 15:52:09 +0600

    fix: add mocks for technical documents

--- factory_dashboard/src/app/pages/station-overview/tech-dock/tech-dock.component.ts
+++ factory_dashboard/src/app/pages/station-overview/tech-dock/tech-dock.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { TechnicalDocuments } from '../../../models/technicalDocuments.model';
+import { TECHNICAL_DOCUMENTS_MOCK } from '../../../mocks/TechnicalDocuments.mock';
 
 @Component({
   standalone: true,
@@ -12,36 +14,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './tech-dock.component.html',
   styleUrl: './tech-dock.component.css',
 })
-export class TechDockComponent {
-  documents = [
-    { id: 'act', name: 'Акт СБ и ПАЗ', fileName: 'akt_sb_paz_1.pdf' },
-    {
-      id: 'document',
-      name: 'ОРД на отключение блокировки',
-      fileName: 'ord_otkl_block_1.pdf',
-    },
-    {
-      id: 'plan',
-      name: 'План мероприятий',
-      fileName: 'plan_meropriyatiy_1.pdf',
-    },
-    {
-      id: 'regulation',
-      name: 'Регламент раздел 5.2',
-      fileName: 'reglament_5_2.pdf',
-    },
-  ];
+export class TechDockComponent implements OnInit{
+  technicalDocuments: TechnicalDocuments[] = [];
 
   selectedDocument: string = '';
 
   constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.loadTechnicalDocumentMockData();
+  }
+
   onDocumentChange(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
-    const documentId = selectElement.value;
+    const documentId = Number(selectElement.value);
 
     if (documentId) {
-      const selectedDoc = this.documents.find((doc) => doc.id === documentId);
+      const selectedDoc = this.technicalDocuments.find((doc) => doc.id === documentId);
       if (selectedDoc) {
         this.downloadFile(selectedDoc.fileName);
       }
@@ -56,19 +45,23 @@ export class TechDockComponent {
     this.http.get(fileUrl, { responseType: 'blob' }).subscribe({
       next: (blob: Blob) => {
         const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
+        const createdElement = document.createElement('a');
 
-        a.href = url;
-        a.download = fileName;
-        document.body.appendChild(a);
-        a.click();
+        createdElement.href = url;
+        createdElement.download = fileName;
+        document.body.appendChild(createdElement);
+        createdElement.click();
 
         window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
+        document.body.removeChild(createdElement);
       },
       error: (error) => {
         console.error('Ошибка при скачивании файла:', error);
       },
     });
+  }
+
+  loadTechnicalDocumentMockData() {
+    this.technicalDocuments = [...TECHNICAL_DOCUMENTS_MOCK];
   }
 }
